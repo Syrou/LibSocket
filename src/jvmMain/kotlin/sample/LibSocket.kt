731142882d66444639827786bfa515eec16ebf9f
Joakim Forslund <syroumusic@gmail.com>
2019-09-23 21:14:31 +0200

    Added base for Java impl

--- src/jvmMain/kotlin/sample/LibSocket.kt
+++ src/jvmMain/kotlin/sample/LibSocket.kt
@@ -1,18 +1,27 @@
 package sample
 
+import java.net.Socket
+import java.io.DataOutputStream
+
 actual class LibSocket actual constructor():Base() {
+    var socket:Socket? = null
     actual fun connect(address:String, port:Int) {
+        socket = Socket(address, port)
     }
 
     actual fun disconnect() {
+        socket?.close()
     }
 
     actual fun recv() {
     }
 
     actual fun send(text: String) {
+        send(text.toByteArray())
     }
 
     actual fun send(byteArray: ByteArray) {
+        val outToServer = DataOutputStream(socket?.getOutputStream())
+        outToServer.write(byteArray, 0 , byteArray.size)
     }
 }
